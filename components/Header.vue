94914e0ee09707a96283adce39c347a2050bb3e8
ivankusuma07 <ivankusuma07@gmail.com>
2022-01-20 09:57:54 +0700

    home page

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="light" variant="light">
-      <b-navbar-brand to="/">NavBar</b-navbar-brand>
+  <div class="wrapper">
+    <b-navbar toggleable="md" type="light" variant="light" fixed="top">
+      <b-navbar-brand to="/" class="mx-3">
+        <div class="d-none d-sm-block">
+          <b-img src="/sahaware.png" alt="Left image" fluid></b-img>
+        </div>
+        <div class="d-xl-none d-md-none d-sm-none">
+          <b-row>
+            <b-col cols="10">
+              <b-img src="/sahaware.png" alt="Left image" fluid></b-img>
+            </b-col>
+          </b-row>
+        </div>
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <b-navbar-nav class="text-center">
+          <b-nav-item to="/">Home</b-nav-item>
           <b-nav-item to="/article">Article</b-nav-item>
           <b-nav-item to="/create">Create</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item @click="Login">Login</b-nav-item>
+        <b-navbar-nav class="ml-auto text-center">
+          <b-nav-item id="btnLogin" @click="Login">Login</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -35,7 +47,7 @@ export default {
   },
 }
 </script>
-<style>
+<style scoped>
 .bg-light {
   background: #ffffff;
   box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
@@ -43,4 +55,17 @@ export default {
 /* .margin {
   margin-right: -30px;
 } */
+.nav-item.nav-item.nav-item a {
+  color: black;
+}
+.nav-item.nav-item.nav-item a.nuxt-link-exact-active.nuxt-link-active {
+  color: #ed3237;
+}
+#btnLogin > .nav-link {
+  color: #2d9cdb;
+}
+
+.navbar-light .navbar-toggler {
+  border-color: rgba(0, 0, 0, 0);
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 <template>
-  <b-container>home</b-container>
+  <div class="bg">
+    <b-container>
+      <b-row>
+        <b-col cols="12" class="p-3" style="margin-top: 20vh">
+          <b-img src="/woman.png" alt="Left image" fluid></b-img>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" class="text-center">
+          <h2>Article</h2>
+        </b-col>
+      </b-row>
+      <b-row class="mt-5 mb-5">
+        <b-col sm>
+          <b-img src="/image1.png" alt="Left image" fluid></b-img>
+          <div class="title text-left">
+            How to create meaningful progress with design?
+          </div>
+          <div class="subtitle text-left">
+            It takes hard work to find the balance between values and aims.
+          </div>
+        </b-col>
+        <b-col sm>
+          <b-img src="/image2.png" alt="Left image" fluid></b-img>
+          <div class="title text-left">
+            Is It Ok to Cheat to be a Better UX/UI Designer?
+          </div>
+          <div class="subtitle text-left">
+            How can cheating in design help you become a better designer?
+          </div>
+        </b-col>
+        <b-col sm>
+          <b-img src="/image3.png" alt="Left image" fluid></b-img>
+          <div class="title text-left">
+            Try peak-end rule on user journey map
+          </div>
+          <div class="subtitle text-left">
+            Designing a user flow requires careful management of resources.
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -7,3 +49,22 @@ export default {
   name: 'IndexPage',
 }
 </script>
+<style scoped>
+.bg {
+  background-color: #ffffff;
+}
+.title {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  margin-bottom: 8px;
+  margin-top: 16px;
+}
+.subtitle {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+}
+</style>
